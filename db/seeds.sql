bf4704e6da386e4442fd83a68ecf20ab8eb235a5
Joey2Jobs <scholz.joe@gmail.com>
2019-03-10 16:08:54 -0500

    folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO (burger_name, devoured) VALUES (Single, True);
+INSERT INTO (burger_name, devoured) VALUES (Double Trouble, False);
+INSERT INTO (burger_name, devoured) VALUES (Tripity Trip, True);
+INSERT INTO (burger_name, devoured) VALUES (Pounder Pounder, False);
